862aca032f3322780cfc9f57781caea65e509901
vovka26 <317110@gmail.com>
2019-03-03 17:22:02 -0500

    by default displays 5 actors and on the button click displays the rest

--- src/containers/ActorsList.js
+++ src/containers/ActorsList.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import ActorCard from './ActorCard';
+import { Button } from 'semantic-ui-react';
 
-const ActorsList = ({actorsList}) => {
-    return ( 
-        <div className='ui grid actors-list'>
-            {actorsList.map(actor => (
-                <ActorCard 
-                    key={actor.id}
-                    actor={actor}
+class ActorsList extends PureComponent {
+    state = {
+        showAll: false
+    }
+
+    handleMoreActors = () => {
+        this.setState({
+            showAll: !this.state.showAll
+        })
+    }
+
+    filterActors = () => {
+        const { actorsList } = this.props
+        if (this.state.showAll ) {
+            return actorsList;
+        }else{
+            return actorsList.slice(0, 5)
+        }
+    }
+
+    render() {
+        
+        return (
+            <div className='ui grid actors-list'>
+                {this.filterActors().map(actor => (
+                    <ActorCard
+                        key={actor.id}
+                        actor={actor}
+                    />
+                ))}
+                <Button 
+                    content={this.state.showAll ? 'Show less' : 'Show all'}
+                    onClick={this.handleMoreActors}
                 />
-            ))}
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 const mapStateToPtops = state => {
