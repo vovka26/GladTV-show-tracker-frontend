49901bc4ed67ee4349e230f081d920a2539f3625
vovka26 <317110@gmail.com>
2019-03-03 03:14:03 -0500

    can search using direct link/on hard refresh

--- src/containers/ShowsList.js
+++ src/containers/ShowsList.js
@@ -1,39 +1,51 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import * as actions from '../redux/actions'
 import ShowCard from './ShowCard';
 import { Dimmer, Loader } from 'semantic-ui-react';
 
-const ShowList = (props) => {
-    const { shows } = props
-    return (
-        shows ?
-            <div className='ui grid centered'>
-                <Route path='/search' render={() => (
-                    shows.map(show =>
-                        <ShowCard
-                            key={show.id}
-                            show={show}
-                        />)
-                )} />
-            </div>
-            :
-            <Dimmer active inverted>
-                <Loader
-                    size='large'
-                    inverted
-                    content='Loading'
-                />
-            </Dimmer>
-    )
+class ShowList extends PureComponent {
+    componentWillMount() {
+        const { location, setSearchTerm, searchTerm, getShows } = this.props
+        const query = location.search.split('?query=')[1]
+        if (!searchTerm && query) {
+            setSearchTerm(query)
+            getShows()
+        }
+    }
 
+    render() {
+        const { shows } = this.props
+        return (
+            shows ?
+                <div className='ui grid centered'>
+                    <Route path='/search' render={() => (
+                        shows.map(show =>
+                            <ShowCard
+                                key={show.id}
+                                show={show}
+                            />)
+                    )} />
+                </div>
+                :
+                <Dimmer active inverted>
+                    <Loader
+                        size='large'
+                        inverted
+                        content='Loading'
+                    />
+                </Dimmer>
+        )
+    }
 }
 
 const mapStateToProps = state => {
     return {
-        shows: state.shows
+        shows: state.shows,
+        searchTerm: state.searchTerm
     }
 }
 
-export default withRouter(connect(mapStateToProps)(ShowList));
+export default withRouter(connect(mapStateToProps, actions)(ShowList));
