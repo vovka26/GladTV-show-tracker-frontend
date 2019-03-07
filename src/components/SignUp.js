3e94c203d653aa89fb95816506ed7040aa368cf2
vovka26 <317110@gmail.com>
2019-03-06 21:46:58 -0500

    decided to add animation to signup too

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../redux/actions';
 
@@ -27,54 +27,59 @@ class SignUp extends PureComponent {
         const { firstName, lastName, username, password } = this.state
         return (
             localStorage.token ? <Redirect to='/watchlist' /> :
-                <Segment
-                    placeholder
-                    className='login-signup'
-                >
-                    <Grid relaxed='very' stackable>
-                        <Grid.Column>
-                            <Form onSubmit={this.onSubmit}>
-                                <h3>Create New User</h3>
-                                <Form.Input
-                                    // label='First Name'
-                                    placeholder='First Name'
-                                    name='firstName'
-                                    value={firstName}
-                                    onChange={this.onChange}
-                                />
-                                <Form.Input
-                                    // label='Last Name'
-                                    placeholder='Last Name'
-                                    name='lastName'
-                                    value={lastName}
-                                    onChange={this.onChange}
-                                />
-                                <Form.Input
-                                    // label='Username'
-                                    placeholder='Username'
-                                    name='username'
-                                    value={username}
-                                    onChange={this.onChange}
-                                />
-                                <Form.Input
-                                    type='password'
-                                    // label='Password'
-                                    placeholder='Password'
-                                    name='password'
-                                    value={password}
-                                    onChange={this.onChange}
-                                />
-                                <Button 
-                                    content='Create' 
-                                    type='submit'
-                                />
-                            </Form>
-                            <div>
-                                Have an account? {<Link to='/login'>Login</ Link>}
-                            </div>
-                        </Grid.Column>
-                    </Grid>
-                </Segment>
+                <Fragment>
+                    <video id="login-background" loop muted autoPlay playsInline poster={'https://static1.squarespace.com/static/58167399d2b857ceb9fd5059/t/5ae7f87b352f53431725b69f/1525151889635/433d83f7e481f35245f8c6bb7c7591d8.gif'}>
+                        <source src={'https://static1.squarespace.com/static/58167399d2b857ceb9fd5059/t/5ae7f87b352f53431725b69f/1525151889635/433d83f7e481f35245f8c6bb7c7591d8.gif'} type="video/mp4" />
+                    </video>
+                    <Segment
+                        placeholder
+                        className='login-signup'
+                    >
+                        <Grid relaxed='very' stackable>
+                            <Grid.Column>
+                                <Form onSubmit={this.onSubmit}>
+                                    <h3>Create New User</h3>
+                                    <Form.Input
+                                        // label='First Name'
+                                        placeholder='First Name'
+                                        name='firstName'
+                                        value={firstName}
+                                        onChange={this.onChange}
+                                    />
+                                    <Form.Input
+                                        // label='Last Name'
+                                        placeholder='Last Name'
+                                        name='lastName'
+                                        value={lastName}
+                                        onChange={this.onChange}
+                                    />
+                                    <Form.Input
+                                        // label='Username'
+                                        placeholder='Username'
+                                        name='username'
+                                        value={username}
+                                        onChange={this.onChange}
+                                    />
+                                    <Form.Input
+                                        type='password'
+                                        // label='Password'
+                                        placeholder='Password'
+                                        name='password'
+                                        value={password}
+                                        onChange={this.onChange}
+                                    />
+                                    <Button
+                                        content='Create'
+                                        type='submit'
+                                    />
+                                </Form>
+                                <div>
+                                    Have an account? {<Link to='/login'>Login</ Link>}
+                                </div>
+                            </Grid.Column>
+                        </Grid>
+                    </Segment>
+                </Fragment>
         )
     }
 }
